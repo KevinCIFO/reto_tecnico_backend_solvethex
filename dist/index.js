a7bf8d75d001687ae579ead0b7c181080357260a
KevinCIFO <kevinlarriega@gmail.com>
2023-05-21 12:54:15 +0200

    Endposints GET, POST PUT y DELETE creados

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Typescript y Node funcionan');
+app.use(express_1.default.json());
+app.get('/api/users', (req, res) => {
+    res.json({ message: 'Lista de usuarios' });
+});
+app.post('/api/user/create', (req, res) => {
+    const { username, email } = req.body;
+    res.json({ message: 'Usuario creado correctamente' });
+});
+app.put('/api/user/:id', (req, res) => {
+    const userId = req.params.id;
+    const { username, email } = req.body;
+    res.json({ message: `Usando con id ${userId} actualizado correctamente` });
+});
+app.delete('/api/user/:id', (req, res) => {
+    const userId = req.params.id;
+    res.json({ message: `Usando con id ${userId} eliminado correctamente` });
 });
 app.listen(port, () => {
     console.log(`Usando el puerto ${port} para este proyecto`);
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,29 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Typescript y Node funcionan')
+app.use(express.json())
+
+app.get('/api/users', (req, res) => {
+    res.json({ message: 'Lista de usuarios' })
+})
+
+app.post('/api/user/create', (req, res) => {
+    const { username, email } = req.body
+
+    res.json({ message: 'Usuario creado correctamente' })
+})
+
+app.put('/api/user/:id', (req, res) => {
+    const userId = req.params.id
+    const { username, email } = req.body
+
+    res.json({ message: `Usando con id ${userId} actualizado correctamente` })
+})
+
+app.delete('/api/user/:id', (req, res) => {
+    const userId = req.params.id
+
+    res.json({ message: `Usando con id ${userId} eliminado correctamente` })
 })
 
 app.listen(port, () => {
